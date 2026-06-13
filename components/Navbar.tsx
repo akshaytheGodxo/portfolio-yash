18effceac1a3389ba846600e16664c89c57ec781
akshaytheGodxo <akshaysinghc230@gmail.com>
2026-06-13 20:24:36 +0530

    navbar visible

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -47,7 +47,7 @@ export default function Navbar() {
       {/* Desktop Nav Links */}
       <div
         style={{
-          display: "none",
+          display: "",
         }}
         className="desktop-nav"
       >
